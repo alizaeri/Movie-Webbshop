8805d4569b2eac6173a35c47bca86a65ad370b69
MOTMTOKEN <amirnilsson3@gmail.com>
2022-05-12 12:33:51 +0200

    added details with styling

--- src/pages/Detailview.js
+++ src/pages/Detailview.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
-
+import "../Detailview.css";
 
 
 export const Detailview = () => {
@@ -11,8 +11,12 @@ export const Detailview = () => {
 
     const [movie, setMovie] = useState({});
 
+    const base = "https://image.tmdb.org/t/p/w500"
+
+
     useEffect(() => {
         fetchMovieDetails(id)
+        window.scrollTo(0, 1000)
           }, [])
     
     const fetchMovieDetails = async (id) => {
@@ -24,8 +28,56 @@ export const Detailview = () => {
 
        
   return (
-    <h1>{movie.title}</h1>
+<div className="detail">
+			<div className="detail__left">
+				<img
+					className="detail__poster"
+					src={`${base}${movie.poster_path}`}
+					alt="movie"
+				/>
+			</div>
+			<div className="detail__right">
+				<div className="detail__one">
+					<h1 className="detail__title" >
+						{movie ? movie.title || movie.name : ""}
+					</h1>
+					<p className="detail__desc">{movie.overview}</p>
+				</div>
+                <div className="detail__data">
+					<p className="detail__pop">
+						Popularity Points : {movie.popularity}
+					</p>
+					<p className="detail__rating">
+						User rating : &nbsp;{`  ${movie.vote_average}/10`}
+					</p>
+					<p className="detail__raters">
+						{" "}
+						{movie.vote_count} ratings {" "}
+					</p>
+					{movie.origin_country ? (
+						<p className="detail__country">
+							Country : {movie.origin_country}
+						</p>
+                        ) : (
+                            ""
+                        )}
+                        {movie.runtime && (
+                            <p className="detail__country">Run-time : {movie.runtime} mins</p>
+                        )}
+                        <p className="detail__release">
+                            Released On :{" "}
+                            {movie.first_air_date || movie.release_date + "  "}
+                        </p>
+                    </div>
+                    <button className="btn btn-added">
+							Add to cart
+						</button>
+                    </div>
+                    </div>
+                        
   )
 }
+
+
 
 export default Detailview;
